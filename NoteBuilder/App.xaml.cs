43a0c209cdcbe8b2323f5472ec1c1da68fa85337
James Frost <JamesF0790@gmail.com>
2023-08-12 21:58:47 +1000

    Set up data to create jsons if they don't exist. Create data model

--- NoteBuilder/App.xaml.cs
+++ NoteBuilder/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,27 @@ namespace NoteBuilder
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string dataFolderPath = "Data";
+
+            CreateMissingJsonFile(dataFolderPath, "Greetings.json");
+            CreateMissingJsonFile(dataFolderPath, "Rules.json");
+            CreateMissingJsonFile(dataFolderPath, "Citations.json");
+            CreateMissingJsonFile(dataFolderPath, "Signoffs.json");
+        }
+
+        private void CreateMissingJsonFile(string folderPath, string fileName)
+        {
+            string filePath = Path.Combine(folderPath, fileName);
+
+            if(!File.Exists(filePath))
+            {
+                Directory.CreateDirectory(folderPath);
+                File.WriteAllText(filePath, "[]");
+            }
+        }
     }
 }
---
+++ NoteBuilder/Model/NoteBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBuilder.Model
+{
+    public class NoteBlock
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid Id { get; set; }
+
+        public NoteBlock(string title, string content, Guid id)
+        {
+            Title = title;
+            Content = content;
+            Id = id;
+        }
+    }
+}
